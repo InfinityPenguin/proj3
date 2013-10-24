8ac8c37e2a7679ede409dad79fa95ebdfa849daa
seanucb <sean.liu@berkeley.edu>
2013-10-23 20:01:34 -0700

    Update part1.c

--- part1.c
+++ part1.c
@@ -4,31 +4,36 @@
 int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
                     float* kernel)
 {
-    /*
+    //NOTE: right now the code only works with matrices of length divisible by four!!!!
+    
+    //flipping the kernel
+    float k[KERNX*KERNY];
+    for (int i = 0; i<KERNX*KERNY/2; i++) {
+	k[i] = kernel[KERNX*KERNY-1-i]; //k is flipped version of kernel
+    }
+
+    //zero pad the matrix "in" and call it "buf". Example: if "in" was 4x4, then copy it onto buf while padding it so that it is 6x6. remember to keep buf as a 1-D array, row-wise implemented
+    
+    
     // the x coordinate of the kernel's center
-    int kern_cent_X = (KERNX - 1)/2;
+    // int kern_cent_X = (KERNX - 1)/2;
     // the y coordinate of the kernel's center
-    int kern_cent_Y = (KERNY - 1)/2;
+    //int kern_cent_Y = (KERNY - 1)/2;
     
     // main convolution loop
-	for(int x = 0; x < data_size_X; x++){ // the x coordinate of the output location we're focusing on
-		for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
-			for(int i = -kern_cent_X; i <= kern_cent_X; i++){ // kernel unflipped x coordinate
-				for(int j = -kern_cent_Y; j <= kern_cent_Y; j++){ // kernel unflipped y coordinate
-					// only do the operation if not out of bounds
-					if(x+i>-1 && x+i<data_size_X && y+j>-1 && y+j<data_size_Y){
-						//Note that the kernel is flipped
-						out[x+y*data_size_X] += 
-								kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(x+i) + (y+j)*data_size_X];
-					}
-				}
-			}
-		}
+    for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
+	for(int x = 0; x + 3 < data_size_X; x+=4){ // the x coordinate of the output location we're focusing on
+	    __m128i n = _mm_loadu_si128((__m128i*) (out+x+y*data_size_X)); //getting the next four values of output matrix
+	    for (int i = 0; i < KERNX*KERNY;i++) { //iterating through kernal	       
+		__m128i kk =  _mm_load1_ps(k+i); //load four of the same value of k[i]
+		__m128i m = _mm_loadu_ps(buf);
+		//	out[x+y*data_size_X] += 
+		//	  kernel[(kern_cent_X-i)+(kern_cent_Y-j)*KERNX] * in[(x+i) + (y+j)*data_size_X];
+		  }
+	  }
 	}
-    */
-
     
-	return 1;
+    return 1;
 
     
 }
