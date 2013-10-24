afd531de704773c6444f0e8a15b181a9c5faed04
seanucb <sean.liu@berkeley.edu>
2013-10-23 22:13:58 -0700

    with padded zeros and sse
    Gflop is around 2

--- part1.c
+++ part1.c
@@ -13,6 +13,12 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     }
 
     //zero pad the matrix "in" and call it "buf". Example: if "in" was 4x4, then copy it onto buf while padding it so that it is 6x6. remember to keep buf as a 1-D array, row-wise implemented
+        float buf[(data_size_X+2)*(data_size_Y+2)]= {0}; //initialize all buffer elements to zero
+    for (int i = 1; i <= data_size_X; i++) {
+	for (int j = 1; j <= data_size_Y; j++) {
+	    buf[i+j*(data_size_X+2)] = in[(i-1)+(j-1)*data_size_X]; //update the elements with in
+	}
+    }
     
     
     // the x coordinate of the kernel's center
@@ -24,11 +30,11 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     for(int y = 0; y < data_size_Y; y++){ // the y coordinate of theoutput location we're focusing on
 	for(int x = 0; x + 3 < data_size_X; x+=4){ // the x coordinate of the output location we're focusing on
 	    float* out_index = out+x+y*data_size_X;
-	    __m128i n = _mm_loadu_si128((__m128i*) (out_index)); //getting the next four values of output matrix
+	    __m128 n = _mm_loadu_ps(out_index); //getting the next four values of output matrix
 	    for (int i = 0; i < KERNX*KERNY;i++) { //iterating through kernal	       
-		__m128i kk =  _mm_load1_ps(k+i); //load four of the same value of k[i]
-		__m128i m = _mm_loadu_ps(buf+(x+i%3)+(y+i/3)*(data_size_X+2)); //load four adjacent values from buf
-		__m128i product = _mm_mul_ps(kk,m);//multiply kk and m
+		__m128 kk =  _mm_load1_ps(k+i); //load four of the same value of k[i]
+		__m128 m = _mm_loadu_ps(buf+(x+i%3)+(y+i/3)*(data_size_X+2)); //load four adjacent values from buf
+		__m128 product = _mm_mul_ps(kk,m);//multiply kk and m
 		n = _mm_add_ps(n, product);//add the product to n
 		_mm_storeu_ps(out_index,n);
 	     }
