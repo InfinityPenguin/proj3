2a730fb3d703c3565207e2047b34a33053020dc2
seanucb <sean.liu@berkeley.edu>
2013-10-24 15:52:46 -0700

    fixed bugs for non-multiples of 4

--- part1.c
+++ part1.c
@@ -32,7 +32,7 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 */
 
     //zero pad the matrix "in" and call it "buf". Example: if "in" was 4x4, then copy it onto buf while padding it so that it is 6x6. remember to keep buf as a 1-D array, row-wise implemented
-    int buf_x = data_size_X+2+data_size_X%4; //account for cases where length is not divisible by 4
+    int buf_x = data_size_X+2+(8-data_size_X%4); //account for cases where length is not divisible by 4
     int buf_y = data_size_Y+2;
     int buf_size = buf_x*buf_y;
     
@@ -55,6 +55,7 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
     // main convolution loop
     int x;
     int y;
+
     for(y = 0; y < data_size_Y; y++){ // the y coordinate of the output location we're focusing on
 	for(x = 0; x + 15 < data_size_X; x+=16){ // the x coordinate of the output location we're focusing on
 	    float* out_index = out+x+y*data_size_X;
@@ -104,10 +105,10 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 	     }
 	    _mm_storeu_ps(out_index,n);
 	}
-	
+	/*
 	if (data_size_X%4!=0) {
 	    float* out_index = out+x+y*data_size_X;
-	    __m128 n = _mm_loadu_ps(out_index);
+	    __m128 n = _mm_loadu_ps(out_index); //
 	    for (int i = 0; i < KERNX*KERNY;i++) { //iterating through kernal	       
 		__m128 kk =  _mm_load1_ps(k+i); //load four of the same value of k[i]
 		__m128 m = _mm_loadu_ps(buf+(x+i%3)+(y+i/3)*(buf_x)); //load four adjacent values from buf
@@ -120,11 +121,19 @@ int conv2D(float* in, float* out, int data_size_X, int data_size_Y,
 	     for (j=0; j<data_size_X%4; j++) {
 		*out_index = tmp[j];
 	     }
-	}
 	
+	}
+	*/
+
+	if (data_size_X%4!=0) {
+	    for (; x<data_size_X; x++) {
+		for (int i=0; i<KERNX*KERNY; i++ ) {
+		    out[x+y*data_size_X] += k[i]*buf[x+i%3+(y+i/3)*buf_x];
+		}
+	    }
+	}
 	}
     
     return 1;
-
     
 }
